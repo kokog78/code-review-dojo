046c135dd0938ace34a70b50ea0898673667ba11
Kornél Simon <kokog.private@gmail.com>
2018-09-27 12:38:39 +0200

    javascript MorseTranslator osztály tesztekkel

---
+++ code-review-dojo-1/src/main/javascript/morse-translator.js
@@ -0,0 +1,104 @@
+const MorseAbc = require('./morse-abc');
+
+module.exports = class MorseTranslator {
+
+    constructor() {
+        this.abc = new MorseAbc();
+    }
+
+    translateToMorseString(text) {
+        let result = '';
+        if (text) {
+            for (let i=0; i<text.length; i++) {
+                const c = text.charAt(i);
+                switch (c) {
+                    case ' ':
+                    case '\t':
+                    case '\n':
+                    case '\r':
+                        // a szóközt kihagyjuk
+                        break;
+                    default:
+                        const morse = this.abc.getMorseCodeByCharacter(c);
+                        if (!morse) {
+                            throw new Error(`Cannot find morse code for: ${c}`);
+                        }
+                        if (result.length > 0) {
+                            result += ' ';
+                        }
+                        result += morse;
+                        break;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Ellenőrzi, hogy a megadott morze sztring érvényes kódokat tartalmaz-e.
+     * A bemenetben a rövid jelek "."-tal, a hosszú jelek "-"-sal szerepelnek.
+     * Az egyes betűk között egy vagy több szóköz, tabulátor vagy sortörés van.
+     * A metódus akkor tér vissza true értékkel, ha a bemenet csak érvényes
+     * morze jeleket, szóközöket, tabulátorokat és sortöréseket tartalmaz.
+     * @param morse a morze jeleket tartalmazó sztring
+     * @return a bemenet érvényes morze kódot tartalmaz?
+     */
+    isValidMorseString(morse) {
+        if (morse === undefined || morse === null) {
+            return false;
+        }
+        const codes = morse.split(/\s/);
+        for (let code of codes) {
+            if (code) {
+                const c = this.abc.getCharacterByMorseCode(code);
+                if (!c) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Ellenőrzi, hogy a megadott szöveg lefordítható-e morzejelekre.
+     * Ez akkor igaz, ha csak lefordítható karaktereket, szóközöket, tabulátorokat és sortöréseket tartalmaz.
+     * @param text a vizsgálandó szöveg
+     * @return a bemenet lefordítható morze kódra?
+     */
+    canBeTranslatedToMorse(text) {
+
+    }
+
+    /**
+     * Visszaadja a morzejeleket tartalmazó tömböt, amit a bemenet alapján fordított.
+     * Ha a bemenet null, undefined vagy üres sztring, üres tömböt ad.
+     * A bemenetben levő szóközt, tabulátort és sortörés karaktereket figyelmen kívül hagyja.
+     * Ha a bemenet ismeretlen karaktereket tartalmaz, akkor hibát dob.
+     * @param text a lefordítandó karaktereket tartalmazó sztring
+     * @return a lefordított morzejelek
+     */
+    translateToMorseCodes(text) {
+
+    }
+
+    /**
+     * Visszaadja a morzejelekhez tartozó betűket.
+     * A bemenetben a rövid jelek "."-tal, a hosszú jelek "-"-sal szerepelnek.
+     * Az egyes betűk között egy vagy több szóköz, tabulátor vagy sortörés van.
+     * @param morse a morze jeleket tartalmazó sztring
+     * @return a lefordított karaktereket tartalmazó sztring
+     */
+    translateToLetters(morse) {
+
+    }
+
+    /**
+     * Visszaadja a morzejelekhez tartozó betűket.
+     * Sosem ad vissza null vagy undefined értéket.
+     * @param morse a morze jeleket tartalmazó tömb
+     * @return a lefordított karaktereket tartalmazó sztring
+     */
+    translateToLetters(...codes) {
+
+    }
+};
---
+++ code-review-dojo-1/src/test/javascript/morse-translator.spec.js
@@ -0,0 +1,70 @@
+const expect  = require('chai').expect;
+const MorseTranslator = require('../../main/javascript/morse-translator');
+
+describe('MorseTranslator', () => {
+
+    const translator = new MorseTranslator();
+
+    describe('translateToMorseString()', () => {
+
+        it('should return empty string for undefined', () => {
+            const result = translator.translateToMorseString();
+            expect(result).to.equals('');
+        });
+
+        it('should return empty string for null', () => {
+            const result = translator.translateToMorseString(null);
+            expect(result).to.equals('');
+        });
+
+        it('should return empty string for empty string', () => {
+            const result = translator.translateToMorseString('');
+            expect(result).to.equals('');
+        });
+
+        it('should translate text to morse', () => {
+            const result = translator.translateToMorseString('ABC');
+            expect(result).to.equals('.- -... -.-.');
+        });
+
+        it('should skip whitespaces', () => {
+            const result = translator.translateToMorseString('\r\nA B\t\n\rC');
+            expect(result).to.equals('.- -... -.-.');
+        });
+
+        it('should throw error for illegal character', () => {
+            expect(() => translator.translateToMorseString('ABC.')).to.throw;
+        });
+
+    });
+
+    describe('isValidMorseString()', () => {
+
+        it('should return false for null', () => {
+            const result = translator.isValidMorseString(null);
+            expect(result).to.be.false;
+        });
+
+        it('should return false for undefined', () => {
+            const result = translator.isValidMorseString();
+            expect(result).to.be.false;
+        });
+
+        it('should return true for empty string', () => {
+            const result = translator.isValidMorseString('');
+            expect(result).to.be.true;
+        });
+
+        it('should return true for valid morse code', () => {
+            const result = translator.isValidMorseString('.- -... -.-.');
+            expect(result).to.be.true;
+        });
+
+        it('should return false for invalid morse code', () => {
+            const result = translator.isValidMorseString('......');
+            expect(result).to.be.false;
+        });
+
+    });
+
+});
